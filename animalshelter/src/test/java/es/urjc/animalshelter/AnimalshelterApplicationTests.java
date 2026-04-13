251d9c847638f57ee036dd2a1ee69b8926effee9
javiviicentee <j.vicente.2023@alumnos.urjc.es>
2026-04-13 10:02:36 +0200

    Initial Spring Boot project setup

---
+++ animalshelter/src/test/java/es/urjc/animalshelter/AnimalshelterApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.animalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalshelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
